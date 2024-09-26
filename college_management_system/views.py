b0fa8e18651d1e2d91e86123085262b0670d6473
Riya20khandelwal <riyakhandelwal204@gmail.com>
2024-09-26 10:18:49 +0530

    email backend

--- college_management_system/views.py
+++ college_management_system/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render
+from student.EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate, login, logout
 # from .settings import BASE_DIR, rr
 
 def BASE(request):
@@ -7,3 +9,10 @@ def BASE(request):
 
 def LOGIN(request):
     return render(request, 'login.html')
+
+def doLogin(request):
+    if request.method == "POST":
+        user = EmailBackEnd.authenticate(request,
+                                         username=request.POST.get('email'),
+                                         )
+        return None
