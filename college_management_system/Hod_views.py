98b14e47b45add3112807cd62c24875159f19223
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-03-05 15:39:27 +0530

    Session Add, View, Edit and Delete

--- college_management_system/Hod_views.py
+++ college_management_system/Hod_views.py
@@ -392,3 +392,68 @@ def DELETE_SUBJECT(request, id):
     subject.delete()
     messages.success(request, "Subject deleted successfully.")
     return redirect('view_subject')
+
+
+
+
+@login_required(login_url='/')
+def ADD_SESSION(request):
+    if request.method == "POST":
+        session_year_start = request.POST.get('session_year_start')
+        session_year_end = request.POST.get('session_year_end')
+
+        session = Session_Year(
+            session_start = session_year_start,
+            session_end = session_year_end,
+        )
+        session.save()
+        messages.success(request, "Session is successfully created.")
+        return redirect('add_session')
+    return render(request, "Hod/add_session.html")
+
+
+@login_required(login_url='/')
+def VIEW_SESSION(request):
+    session = Session_Year.objects.all()
+
+    context = {
+        'session': session,
+    }
+    return render(request, 'Hod/view_session.html', context)
+
+
+
+@login_required(login_url='/')
+def EDIT_SESSION(request, id):
+    session = Session_Year.objects.get(id=id)
+
+    context = {
+        'session': session,
+    }
+    
+    return render(request, "Hod/edit_session.html", context)
+
+
+@login_required(login_url='/')
+def UPDATE_SESSION(request):
+    if request.method == "POST":
+        session_id = request.POST.get('session_id')
+        session_year_start = request.POST.get('session_year_start')
+        session_year_end = request.POST.get('session_year_end')
+
+        session = Session_Year.objects.get(id=session_id)
+        session.session_start = session_year_start
+        session.session_end = session_year_end
+        session.save()
+
+        messages.success(request, "Session is successfully updated.")
+        return redirect('view_session')
+    return render(request, "Hod/edit_session.html")
+
+
+@login_required(login_url='/')
+def DELETE_SESSION(request, id):
+    session = Session_Year.objects.get(id=id)
+    session.delete()
+    messages.success(request, "Session deleted successfully.")
+    return redirect('view_session')
--- college_management_system/urls.py
+++ college_management_system/urls.py
@@ -60,5 +60,12 @@ urlpatterns = [
     path('Hod/Subject/Delete/<str:id>', Hod_Views.DELETE_SUBJECT, name='delete_subject'),
 
 
+    path('Hod/Session/Add', Hod_Views.ADD_SESSION, name='add_session'),
+    path('Hod/Session/View', Hod_Views.VIEW_SESSION, name='view_session'),
+    path('Hod/Session/Edit/<str:id>', Hod_Views.EDIT_SESSION, name='edit_session'),
+    path('Hod/Session/Update', Hod_Views.UPDATE_SESSION, name='update_session'),
+    path('Hod/Session/Delete/<str:id>', Hod_Views.DELETE_SESSION, name='delete_session'),
+
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
