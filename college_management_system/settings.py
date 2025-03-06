d7dcb358f61fd9e60fbc6a6ce489e9f49d63d922
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-03-06 13:45:51 +0530

    Sidebar Link Active and Deactive

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'active_link',
 ]
 
 MIDDLEWARE = [
@@ -94,6 +95,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
